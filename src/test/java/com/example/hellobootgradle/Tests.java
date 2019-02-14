f9e1afb442f718d493ea57ba041f057485745439
xs-cy <chuanyunw@163.com>
2019-02-14 19:50:22 +0800

    Test file submit

---
+++ src/test/java/com/example/hellobootgradle/Tests.java
@@ -0,0 +1,26 @@
+package com.example.hellobootgradle;
+
+
+import com.example.hellobootgradle.pojo.Face;
+import com.example.hellobootgradle.server.FaceService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Tests {
+
+    @Autowired
+    FaceService faceService;
+    @Test
+    public void contextLoads() {
+        List<Face> faceList = faceService.getAllFae();
+        System.out.println("测试："+faceList);
+    }
+
+}
